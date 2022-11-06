b8c5473fc3c734fcbf0d930f00d8904bbc332345
Mariia IVANCHENKO <mariiaivanchenko@mbp-de-mariia.home>
2022-11-06 21:20:53 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Logo from "./images/logo.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+        <header>
+          <img className="logo" src={Logo} alt="logo todolist" />
+          <h1>Todo List</h1>
+        </header>
+        <main>
+          <input type="text" name="put" value="" placeholder="my new task" />
+          <button type="button">Add task</button>
+        </main>
+        <footer>
+          <p>
+            Made with <span>React</span>at <span>Le Reacteur</span> by{" "}
+            <span>Mariia MERCIER</span>
+          </p>
+        </footer>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
